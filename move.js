535cf1b376624c71bcd3db5b3af08dbc9b0af19d
Eric Wang <erwang01@gmail.com>
2016-04-10 11:57:41 -0700

    Started developing for node

---
+++ move.js
@@ -0,0 +1,25 @@
+var SerialPortLib = require("serialport");
+var SerialPort = SerialPortLib.SerialPort;
+var serialPort = new SerialPort('/dev/cu.usbserial-AM01VDHP', {
+  baudRate: 9600,
+  dataBits: 8,
+  parity: 'none',
+  stopBits: 1,
+  flowControl: false,
+  parser: SerialPortLib.parsers.readline("\r\n")
+});
+
+serialPort.on("open", function () {
+  console.log('Serial Port opened');
+
+  serialPort.on('data', function(data) {
+    console.log('data received: ' + data);
+
+  });
+/*
+  serialPort.write(new Buffer('4','ascii'), function(err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });
+  */
+});
