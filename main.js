699d3e5aaf38719f607cd4db905a8378a06b6af9
Nathan Wang <nathan.r.wang@gmail.com>
2016-04-10 19:22:09 +0000

    Add basic socket.io communication

--- main.js
+++ main.js
@@ -11,8 +11,19 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + "/web/index.html");
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
+io.on("connection", function(socket){
+    console.log("User Connected");
+
+    socket.on("channelName", function(data) {
+        console.log("User sent data from channel channelName with data " + data);
+        console.log("Sending data to everyone...");
+
+        io.emit("someData", "Data received: " + data); // note: Use socket.broadcast.emit to send to everyone but sender.
+    });
+
+    socket.on("disconnect", function(){
+        console.log("User Disconnected");
+    });
 });
 
 http.listen(3000, function(){
